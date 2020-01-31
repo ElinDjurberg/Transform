bb79d9e7c110cc62df267b3c1359a0b79ded9456
ElinDjurberg <57451642+ElinDjurberg@users.noreply.github.com>
2020-01-31 16:47:35 +0100

    Add files via upload

---
+++ manhat.m
@@ -0,0 +1,12 @@
+function [d] = manhat(x,y,u,v)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+%d=abs(x-u)+abs(y-v);
+
+mask(n,m) = manhat(n,m,9,9);
+
+figure;imagesc(mask);
+
+M = M.*(mask<=k);
+end
+
